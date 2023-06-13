69bfa750e486c611e1db253088fe131abed5f332
judy-chelangat <judysigilai75@gmail.com>
2023-06-14 01:38:42 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(){
+    const num1=2000
+    const num2=100
+return (num1/num2)
+}
+console.log(divide(2000))
+const square = value => value*value
+const add = (num3,num4)=>{
+    return num3+num4
+}
